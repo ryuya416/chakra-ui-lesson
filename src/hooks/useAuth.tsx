61a6b113972247919245a6be15b922997f5c4f0b
ryuya0416 <67462677+ryuya416@users.noreply.github.com>
2022-06-03 16:21:07 +0900

    ✨ 認証

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,10 +1,12 @@
 import { useRouter } from "next/router";
 import { useCallback, useState } from "react";
+import { useLoginUser } from "./useLoginUser";
 import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
   const router = useRouter();
   const { showMessage } = useMessage();
+  const { setLoginUser } = useLoginUser();
 
   const [loading, setLoading] = useState(false);
 
@@ -18,6 +20,8 @@ export const useAuth = () => {
       console.log(data);
       try {
         if (data) {
+          const isAdmin = data.id === 10 ? true : false;
+          setLoginUser({ ...data, isAdmin });
           showMessage({ title: "ログインしました", status: "success" });
           router.push("/home");
         }
@@ -26,7 +30,7 @@ export const useAuth = () => {
       }
       return data;
     },
-    [router]
+    [router, setLoginUser, showMessage]
   );
   return { login, loading };
 };
